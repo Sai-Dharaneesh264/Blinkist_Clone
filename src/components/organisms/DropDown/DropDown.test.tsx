68925cb37245fcef2bc1c89043eacf58e803fbb9
Sai-Dharaneesh264 <saidharaneesh.gurram@zemosolabs.com>
2022-02-24 11:20:44 +0530

    resolved errors in sonar cloud

--- src/components/organisms/DropDown/DropDown.test.tsx
+++ src/components/organisms/DropDown/DropDown.test.tsx
@@ -3,7 +3,9 @@ import { render } from '@testing-library/react'
 import DropDown from './DropDown'
 
 const handleClose = () => {
-    
+    const x = 'xyz';
+    console.log(x);
+
 }
 it('checks whether component renders properly', () => {
     render(<DropDown handleClose={handleClose} anchorEl={null} />)
